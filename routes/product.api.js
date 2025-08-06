625117a7a4c0fd3bc54cdd0ff2968c54f8e462d6
Mikyung Cho <jmk2633@gmail.com>
2025-08-06 12:26:42 +0900

    input formData

---
+++ routes/product.api.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const authController = require("../controllers/authController");
+const router = express.Router();
+const productController = require("../controllers/productController");
+
+router.post(
+  "/",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  productController.createProduct
+);
+
+module.exports = router;
